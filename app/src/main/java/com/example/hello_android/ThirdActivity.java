da7818190ff70d8110af98e2ff9104646f7a4308
allainrico <allainrico@gmail.com>
2023-03-04 12:17:06 +0800

    new activity

--- app/src/main/java/com/example/hello_android/ThirdActivity.java
+++ app/src/main/java/com/example/hello_android/ThirdActivity.java
@@ -2,13 +2,27 @@ package com.example.hello_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
-
+    public static final String TAG = "ThirdActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button 1 click!");
+                Intent intent = new Intent(ThirdActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
