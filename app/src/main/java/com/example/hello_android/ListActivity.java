56a713f20d8b3cac02cfe93980b5290fead515d7
allainrico <allainrico@gmail.com>
2023-03-04 13:17:59 +0800

    mylist

--- app/src/main/java/com/example/hello_android/ListActivity.java
+++ app/src/main/java/com/example/hello_android/ListActivity.java
@@ -3,6 +3,7 @@ package com.example.hello_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -11,12 +12,16 @@ public class ListActivity extends AppCompatActivity {
     ListView listView;
     TextView textView;
 
+    String[] listitem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
         listView=(ListView)findViewById(R.id.listview);
+        listitem=getResources().getStringArray(R.array.array_technology);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.my_list, R.id.textView, listitem);
 
     }
 }
