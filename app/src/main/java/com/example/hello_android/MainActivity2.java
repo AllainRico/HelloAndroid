bf8bd08b204407f3288efef44b8be81e8fa61f0d
allainrico <allainrico@gmail.com>
2023-03-11 11:32:06 +0800

    11-03-23: ADDED FRAGMENTS

--- app/src/main/java/com/example/hello_android/MainActivity2.java
+++ app/src/main/java/com/example/hello_android/MainActivity2.java
@@ -3,14 +3,20 @@ package com.example.hello_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button firstfragment, secondfragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        firstfragment = (Button) findViewById(R.id.firstfragment);
+        secondfragment = (Button) findViewById(R.id.secondfragment);
     }
+
+
 
 
 }
