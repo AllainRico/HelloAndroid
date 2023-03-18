a8bfb9257ff34440d24abd3dd69d822b4634c62b
allainrico <allainrico@gmail.com>
2023-03-18 12:39:36 +0800

    titlebar added

--- app/src/main/java/com/example/hello_android/MainActivity2.java
+++ app/src/main/java/com/example/hello_android/MainActivity2.java
@@ -1,9 +1,16 @@
 package com.example.hello_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -15,5 +22,48 @@ public class MainActivity2 extends AppCompatActivity {
 
         actionBar.setTitle("Actionbar Title!");
         actionBar.setSubtitle("subtitle!");
+        actionBar.setIcon(R.mipmap.ic_launcher);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+
+        switch (item.getItemId()){
+            case R.id.search:
+                showAlertDialog();
+                break;
+            case R.id.refresh:
+                DialogFragment dialogFragment = new DialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "My Fragment");
+                break;
+            case R.id.copy:
+                Toast.makeText(this, "copy clicked!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert!").setMessage("Danger!");
+
+        builder.setPositiveButton("Yes!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //user clicked ok button
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
